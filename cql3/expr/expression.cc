a0e98f6d6215dccb33f85b14831b6a59bfe82f8c
Avi Kivity <avi@scylladb.com>
2021-08-02 17:14:48 +0300

    cql3: expr: inline fmt::formatter<> integration with expressions
    In order to format expressions from other source files, the
    fmt::formatter<> glue must be exposed.

--- cql3/expr/expression.cc
+++ cql3/expr/expression.cc
@@ -1085,31 +1085,3 @@ std::ostream& operator<<(std::ostream& s, oper_t op) {
 } // namespace expr
 } // namespace cql3
 
-
-template <>
-struct fmt::formatter<cql3::expr::expression> {
-    constexpr auto parse(format_parse_context& ctx) {
-        return ctx.end();
-    }
-
-    template <typename FormatContext>
-    auto format(const cql3::expr::expression& expr, FormatContext& ctx) {
-        std::ostringstream os;
-        os << expr;
-        return format_to(ctx.out(), "{}", os.str());
-    }
-};
-
-template <>
-struct fmt::formatter<cql3::expr::column_value> {
-    constexpr auto parse(format_parse_context& ctx) {
-        return ctx.end();
-    }
-
-    template <typename FormatContext>
-    auto format(const cql3::expr::column_value& col, FormatContext& ctx) {
-        std::ostringstream os;
-        os << col;
-        return format_to(ctx.out(), "{}", os.str());
-    }
-};
